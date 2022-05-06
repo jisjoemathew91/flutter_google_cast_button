de37a2ed6242dcc2da16f1847bdabc5eafde3291
jisjoe-edubites <jj.mathew@edubites.com>
2022-05-06 12:24:04 +0530

    update equatable

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/zonble/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/zonble/KUBE/flutter_google_cast_button/example"
+export "FLUTTER_ROOT=/Users/nirmalkrishnakumar/fvm/versions/2.5.3"
+export "FLUTTER_APPLICATION_PATH=/Users/nirmalkrishnakumar/Documents/Projects/Edubites/flutter_google_cast_button/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/zonble/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=0.2.0"
 export "FLUTTER_BUILD_NUMBER=100"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
